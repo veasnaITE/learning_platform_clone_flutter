7a61d0fd403f7a61ed018c24b16529e5fec263f7
Sean Veasna <veasnavk711@gmail.com>
2023-09-08 11:01:52 +0700

    adding code

---
+++ android/app/src/main/kotlin/com/vsna/learning_platform/learning_platform/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vsna.learning_platform.learning_platform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:learning_platform/screens/welcome_sceen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
